4d4fc5d606d326373f9385379145f1ceace12f4c
codecraft <1157538+codecraft21@users.noreply.github.com>
2021-06-26 16:36:20 +0200

    generated by start.spring.io

---
+++ src/main/java/ch/codecraft/demo/tracing/DemoTracingApplication.java
@@ -0,0 +1,13 @@
+package ch.codecraft.demo.tracing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTracingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTracingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/codecraft/demo/tracing/DemoTracingApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.codecraft.demo.tracing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTracingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
