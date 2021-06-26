174b5ff9dd82479f52d32f62009586231855fdd6
codecraft <1157538+codecraft21@users.noreply.github.com>
2021-06-26 19:14:10 +0200

    added rest call

--- src/main/java/ch/codecraft/demo/tracing/DemoTracingApplication.java
+++ src/main/java/ch/codecraft/demo/tracing/DemoTracingApplication.java
@@ -5,11 +5,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.UUID;
 
@@ -19,21 +21,34 @@ public class DemoTracingApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoTracingApplication.class, args);
     }
+
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
 
 @RestController
 @Slf4j
 @RequiredArgsConstructor
 class DemoController {
-    private final KafkaTemplate<String, String> template;
+    private final RestTemplate restTemplate;
+    private final KafkaTemplate<String, String> kafkaTemplate;
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String home() {
         log.info("Handling home");
-        final var uuid = UUID.randomUUID();
-        template.send("test", "some data " + uuid);
-        log.info("produced data {}", uuid);
+        final var uuid = restTemplate.getForObject("http://localhost:8080/uuid", String.class);
+        kafkaTemplate.send("test", "some data " + uuid);
+        log.info("produced kafka {}", uuid);
         return "Hello World";
+    }
+
+    @GetMapping("/uuid")
+    public String uuid() {
+        final var uuid = UUID.randomUUID();
+        log.info("generated uuid {}", uuid);
+        return uuid.toString();
     }
 }
 
