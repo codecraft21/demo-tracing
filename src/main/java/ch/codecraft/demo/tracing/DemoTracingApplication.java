6fc42fa28cdb3b19d20b8139090889fc5ac79751
codecraft <1157538+codecraft21@users.noreply.github.com>
2021-06-26 16:46:05 +0200

    added spring-cloud-sleuth-otel

--- src/main/java/ch/codecraft/demo/tracing/DemoTracingApplication.java
+++ src/main/java/ch/codecraft/demo/tracing/DemoTracingApplication.java
@@ -1,13 +1,27 @@
 package ch.codecraft.demo.tracing;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoTracingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoTracingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoTracingApplication.class, args);
+    }
 
+}
+
+@RestController
+@Slf4j
+class DemoController {
+
+    @RequestMapping("/")
+    public String home() {
+        log.info("Handling home");
+        return "Hello World";
+    }
 }
